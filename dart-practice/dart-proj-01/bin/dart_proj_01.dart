286b5459942e160f33a4bf4c2240f870f8a3bedf
Student <LAB-205@aptech-msg.edu>
2024-01-20 17:14:46 +0500

    commit-01

---
+++ dart-practice/dart-proj-01/bin/dart_proj_01.dart
@@ -0,0 +1,5 @@
+import 'package:dart_proj_01/dart_proj_01.dart' as dart_proj_01;
+
+void main() {
+  print('Hello world:');
+}
---
+++ dart-practice/dart-proj-01/test/dart_proj_01_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_proj_01/dart_proj_01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
